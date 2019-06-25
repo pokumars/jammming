cc3c58f5948e436c30146d9aae968b87758d98f0
pokumars <ohenebakob@hotmail.com>
2019-06-25 22:34:39 +0300

    pass down search results and render results

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,38 @@ import './App.css';
 import SearchBar from './../SearchBar/SearchBar';
 import SearchResults from'./../SearchResults/SearchResults';
 import Playlist from'./../Playlist/Playlist';
+import { tsConstructorType } from '@babel/types';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div class="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          {/* <Playlist /> */}
-        </div>
+let track1 = {name: 'Senorita', artist: 'shawn Mendes', album: 'Album1', id: 101};
+let track2 = {name: 'Don\'t Pretend', artist: 'Khalid', album: 'SAFE', id: 102};
+let track3 = {name: 'All Love feat. Manu Laudic', artist: 'Biniyam', album: 'The Abyssinian EP', id: 103};
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults:[track1, track2, track3]
+    };
+    
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div class="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults SearchResults={this.state.searchResults} />
+            {/* <Playlist /> */}
+          </div>
+    </div>
   </div>
-</div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Track/Track.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Track.css';
+
+
+class Track extends React.Component {
+    renderAction (){
+       // return (isRemoval? '-' : '+' );
+    }
+
+    render() {
+        return(
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{this.props.track.name}</h3>
+                <p>{this.props.track.artist} | {this.props.track.album}</p>
+            </div>
+            <button className="Track-action">{this.renderAction}</button>
+        </div>
+      );
+    }
+}
+
+export default Track;
